887e85d4d5541ef3a79dbd90f6557b4577baad29
zachary <zacharyberdell@gmail.com>
2021-09-10 09:58:00 -0600

    fixed variable declarations. Mostly finished but needs debugging.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passwordIncluded = [];
+var passwordString;
 
 // Write password to the #password input
 function writePassword() {
@@ -16,19 +18,21 @@ generateBtn.addEventListener("click", writePassword);
 //appends selected elements to the passwordIncluded array and allows programmer to type just the array and a string message
 function passwordAppend(_stringEntry, _valueArray) {
   if(confirm('Would you like to include ' + _stringEntry + '?')){
-    usableCharacters.passwordIncluded.push(_valueArray);
+    passwordIncluded.push(_valueArray);
   }
 }
 
 function generatePassword() {
   //A collection of arrays that include the characters that can be used in the password generation.
-  var usableCharacters = {
+  const usableCharacters = {
     uppercaseLetters: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
     lowercaseLetters: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
     numberCharacters: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
     specialCharacters: [' ', '!', '\"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'],
-    passwordIncluded: []
-  }
+  };
+  //declaration reset
+  passwordIncluded = [];
+  passwordString = "";
   
   //Has the user enter a password length
   var passwordLength = prompt("Choose a password length between 8 and 128");
@@ -46,13 +50,25 @@ function generatePassword() {
   }
 
   //Prompts the user for which characters they would like to include and loops until the passwordIncluded has a length bigger than 0
-  while(usableCharacters.passwordIncluded.length === 0){
+  while(passwordIncluded.length === 0){
     //runs the passwordAppend function for each array
     passwordAppend('uppercase letters', usableCharacters.uppercaseLetters);
+    passwordAppend('lowercase letters', usableCharacters.lowercaseLetters);
+    passwordAppend('numbers', usableCharacters.numberCharacters);
+    passwordAppend('special characters', usableCharacters.specialCharacters);
 
+    //Sends an alert reminding the user to select at least one option if none where selected
+    if(passwordIncluded.length === 0){
+      alert('Please select at least one option');
+    }
   }
 
+  //appends a randomly picked character to the end of the password string for the amount of times the user selected as the length.
   for(var i = passwordLength; i === 0; i--){
-    
+    passwordString += passwordIncluded[Math.floor(Math.random(0, passwordIncluded.length-1))];
   }
+  console.log(passwordIncluded[Math.floor(Math.random(0, passwordIncluded.length-1))]);
+  console.log(passwordString);
+
+  return passwordString;
 }
