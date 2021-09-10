34ee180e012d9480a9d20031cca4328c3d44d2cd
zachary <zacharyberdell@gmail.com>
2021-09-10 11:43:39 -0600

    Fixed the array merging. String merging stil being worked on.

--- script.js
+++ script.js
@@ -17,18 +17,18 @@ generateBtn.addEventListener("click", writePassword);
 
 //appends selected elements to the passwordIncluded array and allows programmer to type just the array and a string message
 function passwordAppend(_stringEntry, _valueArray) {
-  if(confirm('Would you like to include ' + _stringEntry + '?')){
-    passwordIncluded.push(_valueArray);
+  if(confirm("Would you like to include " + _stringEntry + "?")){
+    passwordIncluded = passwordIncluded.concat(_valueArray);
   }
 }
 
 function generatePassword() {
   //A collection of arrays that include the characters that can be used in the password generation.
   const usableCharacters = {
-    uppercaseLetters: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
-    lowercaseLetters: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
-    numberCharacters: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
-    specialCharacters: [' ', '!', '\"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'],
+    uppercaseLetters: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+    lowercaseLetters: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+    numberCharacters: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+    specialCharacters: [" ", "!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"],
   };
   //declaration reset
   passwordIncluded = [];
@@ -45,29 +45,29 @@ function generatePassword() {
     }
 
     else {
-      var passwordLength = prompt('Please enter a valid input between 8 and 128. Make sure it is a number and you don\'t have a typing error.');
+      var passwordLength = prompt("Please enter a valid input between 8 and 128. Make sure it is a number and you don\'t have a typing error.");
     }
   }
 
   //Prompts the user for which characters they would like to include and loops until the passwordIncluded has a length bigger than 0
   while(passwordIncluded.length === 0){
     //runs the passwordAppend function for each array
-    passwordAppend('uppercase letters', usableCharacters.uppercaseLetters);
-    passwordAppend('lowercase letters', usableCharacters.lowercaseLetters);
-    passwordAppend('numbers', usableCharacters.numberCharacters);
-    passwordAppend('special characters', usableCharacters.specialCharacters);
+    passwordAppend("uppercase letters", usableCharacters.uppercaseLetters);
+    passwordAppend("lowercase letters", usableCharacters.lowercaseLetters);
+    passwordAppend("numbers", usableCharacters.numberCharacters);
+    passwordAppend("special characters", usableCharacters.specialCharacters);
 
     //Sends an alert reminding the user to select at least one option if none where selected
     if(passwordIncluded.length === 0){
-      alert('Please select at least one option');
+      alert("Please select at least one option");
     }
   }
 
   //appends a randomly picked character to the end of the password string for the amount of times the user selected as the length.
   for(var i = passwordLength; i === 0; i--){
-    passwordString += passwordIncluded[Math.floor(Math.random(0, passwordIncluded.length-1))];
+    passwordString += passwordIncluded[Math.floor(Math.random(0, passwordIncluded.length))];
   }
-  console.log(passwordIncluded[Math.floor(Math.random(0, passwordIncluded.length-1))]);
+  console.log(passwordIncluded);
   console.log(passwordString);
 
   return passwordString;
